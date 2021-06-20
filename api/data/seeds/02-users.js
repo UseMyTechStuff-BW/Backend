0e18a58118f119d53432cc582b8f45f411599ab0
alvin-joseph <aznman1234@gmail.com>
2021-06-20 04:42:49 -0500

    heroku push

---
+++ api/data/seeds/02-users.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs")
+
+exports.seed = function (knex) {
+  return knex("users")
+    .del()
+    .then(function () {
+      return knex("users").insert([
+        {
+          username: "Sonic",
+          password: bcrypt.hashSync("1234", 8),
+          role: "renter",
+        },
+        {
+          username: "Mario",
+          password: bcrypt.hashSync("1234", 8),
+          role: "owner",
+        },
+        {
+          username: "Luigi",
+          password: bcrypt.hashSync("1234", 8),
+          role: "renter",
+        },
+      ])
+    })
+}
--- api/server.js
+++ api/server.js
@@ -2,9 +2,26 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRouter = require("./auth/auth-router.js")
+const usersRouter = require("./users/users-router")
+const equipmentRouter = require("./equipment/equipment-router")
+const requestsRouter = require("./requests/requests-router")
+
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+server.use("/api/auth", authRouter)
+server.use("/api/users", usersRouter)
+server.use("/api/equipment", equipmentRouter)
+server.use("/api/requests", requestsRouter)
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+})
 
 module.exports = server
