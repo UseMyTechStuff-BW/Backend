9df115705f752f980cfd641c3443c64e5c857266
alvin-joseph <aznman1234@gmail.com>
2021-06-22 18:03:44 -0500

    edit server.js

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const authRouter = require("./auth/auth-router.js")
 const usersRouter = require("./users/users-router")
 const equipmentRouter = require("./equipment/equipment-router")
-//const requestsRouter = require("./requests/requests-router")
+const requestsRouter = require("./requests/requests-router")
 
 const server = express()
 server.use(express.json())
@@ -15,7 +15,7 @@ server.use(cors())
 server.use("/api/auth", authRouter)
 server.use("/api/users", usersRouter)
 server.use("/api/equipment", equipmentRouter)
-//server.use("/api/requests", requestsRouter)
+server.use("/api/requests", requestsRouter)
 
 server.use((err, req, res, next) => {// eslint-disable-line
     res.status(err.status || 500).json({
